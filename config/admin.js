9573956e0ed9480ad453f3eae7e95949405adae0
Classic_rd <romkery@gmail.com>
2024-05-20 20:35:36 +0700

    feat: added a production database, environments are now separated

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'kZwexojKKkvkHErw6Gce/g=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'kZwexojKKkvkHErw6Gce/g=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'kZwexojKKkvkHErw6Gce/g==s'),
     },
   },
   flags: {
--- config/server.js
+++ config/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 5000),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['+5JAa3CtkmR2e8NsmOtaoA==','gUEm7m0QWRJDqkYuNc6qbw==','Vvc85RMvjTdqT2ZDdu81Rw==','Ovat915qHHTREMuEf6EpEQ==']
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
